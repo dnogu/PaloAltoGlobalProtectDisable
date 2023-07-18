78ac0f9b4cadbb64905a1cc0958a8412a047bacb
dnogu <dnogueira85@gmail.com>
2023-07-18 10:15:30 -0400

    create readme and docstrings

--- disable.py
+++ disable.py
@@ -1,25 +1,46 @@
-import httpx
-import xmltodict
-import json
 import os
 import re
-
-
-
+import json
+import httpx
+import xmltodict
 
 def get_ticket():
-    request_failed = False
-    api_key = os.getenv('SHARED_PANORAMA_API_KEY')
+    """
+    Function to retrieve a ticket from a remote server using an API key. 
+
+    Environment variables:
+    PANORAMA_API_KEY : str : the API key for accessing the server
+    GP_TICKET : str : the raw ticket number
+    DURATION_IN_MINUTES : str : the requested duration for the ticket 
+    portal_name : str : the name of the portal to access
+    template_name : str : the template name for the request
+    BASE_URL : str : the base URL for the API server
+
+    Returns:
+    disable_ticket : str : the retrieved ticket or an error message
+    request_failed : bool : a flag indicating whether the request failed
+    """
+
+    api_key = os.getenv('PANORAMA_API_KEY')
     raw_ticket_number = os.getenv('GP_TICKET').upper()
-    first_half_ticket = re.fandall(r"^....", raw_ticket_number)
-    back_half_ticket = re.fandall(r"....$", raw_ticket_number)
+
+    first_half_ticket = re.findall(r"^....", raw_ticket_number)
+    back_half_ticket = re.findall(r"....$", raw_ticket_number)
+    
     ticket_number = f'{first_half_ticket[0]}-{back_half_ticket[0]}'
     duration = os.getenv('DURATION_IN_MINUTES')
     portal_name = os.getenv('portal_name')
     template_name = os.getenv('template_name')
     base_url = os.getenv('BASE_URL')
-    ticket_url = f'/api/?type=op&cmd=<request><global-protect-portal><ticket><tpl>{template_name}</tpl><duration>{duration}</duration><portal>{portal_name}</portal><request></request>{ticket_number}</ticket></global-protect-portal></request>&key={api_key}'
+    
+    ticket_url = (
+        f"/api/?type=op&cmd=<request><global-protect-portal>"
+        f"<ticket><tpl>{template_name}</tpl><duration>{duration}</duration>"
+        f"<portal>{portal_name}</portal><request></request>{ticket_number}"
+        f"</ticket></global-protect-portal></request>&key={api_key}"
+    )
 
+    # Note: Suppressing SSL warnings can be dangerous. Consider providing a path to a cert file.
     r = httpx.get(f'{base_url}{ticket_url}', verify=False)
 
     if 'request is invalid' in r.text:
@@ -30,4 +51,6 @@ def get_ticket():
         print(r.text)
         ticket_response = json.loads(json.dumps(xmltodict.parse(r.text)))['response']['result']
         disable_ticket = re.search(r"\S*$", ticket_response).group()
+        request_failed = False
+
     return disable_ticket, request_failed
